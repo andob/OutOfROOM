698a13d6937d1e626f89b508521c93d36acf499a
Andrei Dobrescu <usingvirtualbox@gmail.com>
2021-05-07 15:19:56 +0300

    bugfix

--- common-dml/src/main/java/ro/andob/outofroom/ArrayUtils.java
+++ common-dml/src/main/java/ro/andob/outofroom/ArrayUtils.java
@@ -3,9 +3,9 @@ package ro.andob.outofroom;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 import java.util.ArrayList;
-import java.util.LinkedList;
 import java.util.List;
 
+@SuppressWarnings({ "unchecked", "rawtypes" })
 public class ArrayUtils
 {
     public static @NotNull String[] convertObjectArrayToStringArray(@Nullable Object[] objects)
@@ -15,47 +15,70 @@ public class ArrayUtils
 
         String[] strings=new String[objects.length];
         for (int i=0; i<objects.length; i++)
-            strings[i]=convertObjectToString(objects[i]);
+            strings[i]=objToString(objects[i]);
         return strings;
     }
 
-    private static String convertObjectToString(Object object)
+    private static String objToString(Object object)
     {
         if (object==null)
             return "";
 
         if (object.getClass().isArray())
-        {
-            StringBuilder builder=new StringBuilder();
-            Object[] array=(Object[])object;
-
-            for (int i=0; i<array.length; i++)
-            {
-                builder.append(convertObjectToString(array[i]));
-                if (i!=array.length-1)
-                    builder.append(",");
-            }
-
-            return builder.toString();
-        }
+            return arrayToString((Object[])object);
 
         if (object instanceof Iterable)
-        {
-            StringBuilder builder=new StringBuilder();
-            List<Object> list=new ArrayList<>();
-            for (Object item : (Iterable)object)
-                list.add(item);
-
-            for (int i=0; i<list.size(); i++)
-            {
-                builder.append(convertObjectToString(list.get(i)));
-                if (i!=list.size()-1)
-                    builder.append(",");
-            }
-
-            return builder.toString();
-        }
+            return iterableToString((Iterable)object);
 
         return object.toString();
+    }
+
+    private static String arrayToString(Object[] array)
+    {
+        if (array==null)
+            return "";
+        if (array.length==0)
+            return "";
+
+        StringBuilder builder=new StringBuilder();
+        builder.append('(');
+        for (int i=0; i<array.length; i++)
+        {
+            builder.append('\'');
+            builder.append(objToString(array[i]));
+            builder.append('\'');
+            if (i!=array.length-1)
+                builder.append(',');
+        }
+
+        builder.append(')');
+        return builder.toString();
+    }
+
+    private static String iterableToString(Iterable iterable)
+    {
+        if (iterable==null)
+            return "";
+
+        List list=new ArrayList();
+        for (Object item : iterable)
+            list.add(item);
+
+        if (list.isEmpty())
+            return "";
+
+        StringBuilder builder=new StringBuilder();
+        builder.append('(');
+        for (int i=0; i<list.size(); i++)
+        {
+            builder.append('\'');
+            builder.append(objToString(list.get(i)));
+            builder.append('\'');
+            if (i!=list.size()-1)
+                builder.append(',');
+        }
+
+        builder.append(')');
+        return builder.toString();
     }
 }
