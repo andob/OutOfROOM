e61009e0b4a9ff1d8a561d69db3195e767245124
Dobrescu Andrei <usingvirtualbox@gmail.com>
2022-10-17 10:08:18 +0300

    index on multiple columns

--- common-ddl/src/main/java/ro/andob/outofroom/Index.kt
+++ common-ddl/src/main/java/ro/andob/outofroom/Index.kt
@@ -3,11 +3,20 @@ package ro.andob.outofroom
 class Index
 (
     private val table : Table,
-    private val column : Column,
+    private val columns : Array<Column>,
     private val unique : Boolean = false,
 )
 {
-    fun toCreateIndexSQL() = "create ${if(unique) "unique" else ""} index if not exists `index_${table}_$column` on `$table`(`$column`)"
+    constructor(table : Table, column : Column, unique : Boolean = false) :
+        this(table = table, columns = arrayOf(column), unique = unique)
+
+    fun toCreateIndexSQL() : String
+    {
+        val indexName="index_${table}_${columns.joinToString(separator = "_")}"
+        val columnsString=columns.joinToString(separator = ",") { "`$it`" }
+        val uniqueString=if(unique) "unique" else ""
+        return "create $uniqueString index if not exists `$indexName` on `$table`($columnsString)"
+    }
 
     override fun toString() = toCreateIndexSQL()
 }
--- sample/src/main/java/ro/andob/outofroom/sample/App.kt
+++ sample/src/main/java/ro/andob/outofroom/sample/App.kt
@@ -3,8 +3,6 @@ package ro.andob.outofroom.sample
 import android.annotation.SuppressLint
 import android.app.Application
 import ro.andreidobrescu.declarativeadapterkt.listeners.CellViewGlobalEvents
-import ro.andreidobrescu.declarativeadapterkt.listeners.OnCellViewInflatedListener
-import ro.andreidobrescu.declarativeadapterkt.view.CellView
 import ro.andreidobrescu.viewbinding_compat.ReflectiveViewBindingFieldSetter
 
 class App : Application()
